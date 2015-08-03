b3243f14e7ca2e70cabde6d5f5bc8f10fbb15ee1
TamirCroll <tamir@soluto.com>
2015-08-03 19:17:42 +0300

    Support specific failures

--- Daily/ReportBuilder.cs
+++ Daily/ReportBuilder.cs
@@ -9,7 +9,6 @@ namespace Daily
 {
     internal class ReportBuilder
     {
-        private const int BUILD_NAME = 0;
         private const int FAILED = 0;
         private const int SUCCESS = 1;
         private const int IGNORED = 2;
@@ -21,23 +20,22 @@ namespace Daily
             var testsCountByResults = new List<int> { 0, 0, 0 };
             var errorsToTests = new Dictionary<string, List<string>>();
             List<string>  passList= new List<string>();
-            int[] sum = { 0, 0, 0 };
+            int[] sumByFirstLine = { 0, 0, 0 };
 
             addFilesToList(files);
             output.Add(string.Format("DateTime: {0}\n", DateTime.Now.ToString("dd/MM/yyy")));
 
             foreach (var file in files)
             {
-                if (file.Count > 0)
+                if (file.Count > 3)
                 {
-                    errorsToTests.Add(file[BUILD_NAME], new List<string>());
-                    SumFirstLines(file[1], sum);
+                    SumFirstLines(file[3], sumByFirstLine);
                     AddFailures(file, testsCountByResults, errorsToTests, passList);
                 }
             }
 
             int all = testsCountByResults[FAILED] + testsCountByResults[SUCCESS] + testsCountByResults[IGNORED];
-            int all2 = sum[FAILED] + sum[SUCCESS] + sum[IGNORED];
+            int all2 = sumByFirstLine[FAILED] + sumByFirstLine[SUCCESS] + sumByFirstLine[IGNORED];
             int coverage = (all > 0) ? (testsCountByResults[FAILED] + testsCountByResults[SUCCESS])*100/all : 0;
             var sb = new StringBuilder();
 
@@ -48,8 +46,8 @@ namespace Daily
             sb.AppendFormat("Coverage: {0}% \n", coverage);
             output.Add(sb.ToString());
 
-            output.Add("By first line: All: " + all2 + " Failed: " + sum[0] + " Success: " + sum[1] + " Ignored: " +
-                       sum[2] + "\n");
+            output.Add("By first line: All: " + all2 + " Failed: " + sumByFirstLine[0] + " Success: " + sumByFirstLine[1] + " Ignored: " +
+                       sumByFirstLine[2]);
 
             foreach (KeyValuePair<string, List<string>> errorToTests in errorsToTests)
             {
@@ -57,7 +55,7 @@ namespace Daily
                 var errorName = errorToTests.Key;
                 var testNames = errorToTests.Value;
 
-                output.Add(string.Format("{0}: ", errorName));
+                output.Add(string.Format("\n{0}: ", errorName));
                 foreach (string testName in testNames)
                 {
                     output.Add(string.Format("         {0}. {1}", testsCounter++, testName));
@@ -67,7 +65,7 @@ namespace Daily
             File.WriteAllLines("c:/DailyReport/output.txt", output);
         }
 
-        private static void AddFailures(List<string> lines, List<int> testsCount, Dictionary<string, List<string>> errors, List<string> passList)
+        private void AddFailures(List<string> lines, List<int> testsCount, Dictionary<string, List<string>> errors, List<string> passList)
         {
             for (int i = 0; i < lines.Count; i++)
             {
@@ -91,11 +89,12 @@ namespace Daily
                     {
                         string test = lines[i + 2].Replace("ERROR: Test failed: ", "");
                         test += " (" + lines[0] + ")";
-                        i += 4;
+                        i += linesToAddToGetError(lines, i);
                         
                         string error = lines[i].Replace(" + ","");
                         Regex rgx = new Regex("[a-zA-Z]+\\.[a-zA-Z]+\\.");
-                        error = rgx.Replace(error, "");
+                        error = rgx.Replace(error, ""). Replace("Test exception: ", "");
+                        test += setErrorName(ref error);
                         if (!errors.ContainsKey(error))
                         {
                             errors.Add(error, new List<string>());
@@ -111,8 +110,38 @@ namespace Daily
             }
         }
 
+        private string setErrorName(ref string error)
+        {
+            string addToEndOfTestName = "";
+            if (error.Contains(@"concurrent.TimeoutException: Waiter Condition: AnalyticsFetcherWaitCondition Timed out while waiting for:"))
+            {
+                addToEndOfTestName = "[" + error.Replace(@"concurrent.TimeoutException: Waiter Condition: AnalyticsFetcherWaitCondition Timed out while waiting for: ", "") + "]";
+                error =  "TimeoutException: Waiter Condition: AnalyticsFetcherWaitCondition";
+            }
+            if (error.Contains("concurrent.TimeoutException: Waiter Condition:  Wait condition failed. Exception: NoSuchElementException: Couldn't find notification element by predicate: "))
+            {
+                addToEndOfTestName =
+                    error.Replace("concurrent.TimeoutException: Waiter Condition:  Wait condition failed. Exception: NoSuchElementException: Couldn't find notification element by predicate: ", "")
+                    .Replace(" Timed out while waiting for: get notification if shown.", "");
+                error = "TimeoutException: NoSuchElementException: Couldn't find notification element by predicate";
+            }
 
-        private static void SumFirstLines(string firstLine, int[] sum)
+            return addToEndOfTestName;
+        }
+
+        private int linesToAddToGetError(List<string> lines, int i)
+        {
+            if (lines[i + 4].Contains(
+                "RuntimeException: Test initialization failed: Unable to provision, see the following errors"))
+            {
+                return 8;
+            }
+
+            return 4;
+        }
+
+
+        private void SumFirstLines(string firstLine, int[] sum)
         {
             string str = firstLine;
             var match = Regex.Match(str, @".*failed: (\d+).*passed: (\d+).*ignored: (\d+).*");
@@ -130,7 +159,7 @@ namespace Daily
 
 
 
-        private static void addFilesToList(List<List<string>> files)
+        private void addFilesToList(List<List<string>> files)
         {
             files.Add(
                 new List<string>(
@@ -139,10 +168,6 @@ namespace Daily
             files.Add(
                 new List<string>(
                     new List<string> {"FirstExperience"}.Concat(File.ReadAllLines("c:/DailyReport/FirstExperience.txt",
-                        Encoding.UTF8))));
-            files.Add(
-                new List<string>(
-                    new List<string> {"MultipleDevices"}.Concat(File.ReadAllLines("c:/DailyReport/MultipleDevices.txt",
                         Encoding.UTF8))));
             files.Add(
                 new List<string>(
--- Daily/TestsAnalyzer.cs
+++ Daily/TestsAnalyzer.cs
@@ -1,42 +1,103 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
+﻿using System;
+using System.Net;
+using System.Net.Mail;
 
 namespace Daily
 {
     internal class TestsAnalyzer
     {
-        public void Analize()
+        string server = "my.smtp.exampleserver.net";
+
+        public void CreateTestMessage2()
         {
+            try
+            {
+                SmtpClient mySmtpClient = new SmtpClient("my.smtp.exampleserver.net");
 
-            List<List<string>> files = new List<List<string>>();
+                // set smtp-client with basicAuthentication
+                mySmtpClient.UseDefaultCredentials = false;
+                System.Net.NetworkCredential basicAuthenticationInfo = new
+                    System.Net.NetworkCredential("username", "password");
+                mySmtpClient.Credentials = basicAuthenticationInfo;
 
-            files.Add(
-                new List<string>(
-                    new List<string> {"TechnicianView-RealDevice"}.Concat(
-                        File.ReadAllLines("c:/DailyReport/TechnicianView-RealDevice.txt", Encoding.UTF8))));
-            files.Add(
-                new List<string>(
-                    new List<string> {"FirstExperience"}.Concat(File.ReadAllLines("c:/DailyReport/FirstExperience.txt",
-                        Encoding.UTF8))));
-            files.Add(
-                new List<string>(
-                    new List<string> {"MultipleDevices"}.Concat(File.ReadAllLines("c:/DailyReport/MultipleDevices.txt",
-                        Encoding.UTF8))));
-            files.Add(
-                new List<string>(
-                    new List<string> {"OngoingValue"}.Concat(File.ReadAllLines("c:/DailyReport/OngoingValue.txt",
-                        Encoding.UTF8))));
-            files.Add(
-                new List<string>(
-                    new List<string> {"TechExpertExperienceTests"}.Concat(
-                        File.ReadAllLines("c:/DailyReport/techExpertExperienceTests.txt", Encoding.UTF8))));
+                // add from,to mailaddresses
+                MailAddress from = new MailAddress("tamir@soluto.com", "TestFromName");
+                MailAddress to = new MailAddress("tamir@soluto.com", "TestToName");
+                MailMessage myMail = new System.Net.Mail.MailMessage(from, to);
 
-            var output2 = new List<string>();
+                // add ReplyTo
+                MailAddress replyto = new MailAddress("tamir@soluto.com");
+                myMail.ReplyTo = replyto;
 
+                // set subject and encoding
+                myMail.Subject = "Test message";
+                myMail.SubjectEncoding = System.Text.Encoding.UTF8;
 
-            File.WriteAllLines("c:/DailyReport/output.txt", output2);
+                // set body-message and encoding
+                myMail.Body = "<b>Test Mail</b><br>using <b>HTML</b>.";
+                myMail.BodyEncoding = System.Text.Encoding.UTF8;
+                // text or html
+                myMail.IsBodyHtml = true;
+
+                mySmtpClient.Send(myMail);
+            }
+
+            catch (SmtpException ex)
+            {
+                throw new ApplicationException
+                    ("SmtpException has occured: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public void RetryIfBusy(string server)
+        {
+            MailAddress from = new MailAddress("tamir@soluto.com");
+            MailAddress to = new MailAddress("tamir@soluto.com");
+            MailMessage message = new MailMessage(from, to);
+            // message.Subject = "Using the SmtpClient class.";
+            message.Subject = "Using the SmtpClient class.";
+            message.Body = @"Using this feature, you can send an e-mail message from an application very easily.";
+            // Add a carbon copy recipient.
+            MailAddress copy = new MailAddress("Notifications@contoso.com");
+            message.CC.Add(copy);
+            SmtpClient client = new SmtpClient(server);
+            // Include credentials if the server requires them.
+            client.Credentials = (ICredentialsByHost)CredentialCache.DefaultNetworkCredentials;
+            Console.WriteLine("Sending an e-mail message to {0} using the SMTP host {1}.",
+                 to.Address, client.Host);
+            try
+            {
+                client.Send(message);
+            }
+            catch (SmtpFailedRecipientsException ex)
+            {
+                for (int i = 0; i < ex.InnerExceptions.Length; i++)
+                {
+                    SmtpStatusCode status = ex.InnerExceptions[i].StatusCode;
+                    if (status == SmtpStatusCode.MailboxBusy ||
+                        status == SmtpStatusCode.MailboxUnavailable)
+                    {
+                        Console.WriteLine("Delivery failed - retrying in 5 seconds.");
+                        System.Threading.Thread.Sleep(5000);
+                        client.Send(message);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed to deliver message to {0}",
+                            ex.InnerExceptions[i].FailedRecipient);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in RetryIfBusy(): {0}",
+                        ex.ToString());
+            }
         }
     }
 }
